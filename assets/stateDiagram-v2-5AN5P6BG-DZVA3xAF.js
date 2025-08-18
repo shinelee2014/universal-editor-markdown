e55aa4dabd36fe15b12476a894e52940d952bbab
MeikoChang <128445997+meikochang@users.noreply.github.com>
2025-08-18 22:11:31 +0800

    Initial commit - Universal Editor

---
+++ assets/stateDiagram-v2-5AN5P6BG-DZVA3xAF.js
@@ -0,0 +1,1 @@
+import{s as t,b as r,a,S as s}from"./chunk-OW32GOEJ-DvB4kIBK.js";import{_ as i}from"./index-BxrWwcd6.js";import"./chunk-BFAMUDN2-BuK1_qdb.js";import"./chunk-SKB7J2MH-CuJj9Gxz.js";var l={parser:a,get db(){return new s(2)},renderer:r,styles:t,init:i(e=>{e.state||(e.state={}),e.state.arrowMarkerAbsolute=e.arrowMarkerAbsolute},"init")};export{l as diagram};
